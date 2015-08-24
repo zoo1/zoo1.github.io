8b62c8bac65e33494a9e88cf32ab1599337ac12d
Zachary Ovington <zoo1@pitt.edu>
2015-08-24 17:49:10 -0400

    missed some things

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready( function() {
+	if(window.location.href.indexOf("#res") > -1)
+	{
+		showresume();
+	}
+	else if(window.location.href.indexOf("#port") > -1)
+	{
+		showportfolio();
+	}
+	else
+	{
+		showsummary();
+	}
+	$(".resume").click(showresume);
+	$(".portfolio").click(showportfolio);
+	$(".summary").click(showsummary);
+	function showresume()
+	{
+		$(".resume").addClass("select");
+		$(".portfolio").removeClass("select");
+		$(".summary").removeClass("select");
+		$("#summary").hide();
+		$("#resume").show();
+	}
+
+	function showportfolio()
+	{
+		$(".portfolio").addClass("select");
+		$(".summary").removeClass("select");
+		$(".resume").removeClass("select");
+		$("#summary").hide();
+		$("#resume").hide();
+	}
+	function showsummary()
+	{
+		$(".summary").addClass("select");
+		$(".resume").removeClass("select");
+		$(".portfolio").removeClass("select");
+		$("#summary").show();
+		$("#resume").hide();
+	}
+});
